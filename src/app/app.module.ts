f30bff3cb0655e8ee035faa9505fd4fa23a9fb6c
SWAGAT KUMAR SWAIN <swagat@cognissolutions.com>
2020-11-18 20:58:40 +0530

    adds home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
 import { HomeComponent } from "./components/home.component";
+import { ProductComponent } from "./components/product/product.component";
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, ProductComponent],
   imports: [BrowserModule],
   //providers: [],
   bootstrap: [HomeComponent],
