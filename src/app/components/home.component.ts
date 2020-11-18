9e8390ff459e33ea017b4dd150576846195d04ba
SWAGAT KUMAR SWAIN <swagat@cognissolutions.com>
2020-11-18 20:43:35 +0530

    adds ng-bootstrap

--- src/app/components/home.component.ts
+++ src/app/components/home.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-home",
   template: `
-    <h2>Hello From Home</h2>
+    <h2 class="text-primary">Hello From Home</h2>
     <img src="assets/images/map.png" width="50%" />
   `,
 })
