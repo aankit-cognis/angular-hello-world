6cba338a922fe88a885a2a36fcf53ed14bf6e51e
SWAGAT KUMAR SWAIN <swagat@cognissolutions.com>
2020-11-21 12:49:56 +0530

    ngon changes

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -32,39 +32,39 @@ export class ProductListComponent implements OnInit {
       isActive: true,
       imageUrl: "https://via.placeholder.com/150?text=CD100SS",
     },
-    {
-      productName: "Honda Hornet",
-      description: "A sports Bike",
-      releaseDate: "10-08-2010",
-      price: 200,
-      isActive: true,
-      imageUrl: "https://via.placeholder.com/150?text=Hornet",
-    },
-    {
-      productName: "Super splendor",
-      description: null,
-      releaseDate: "10-08-1980",
-      price: 75,
-      isActive: true,
+    // {
+    //   productName: "Honda Hornet",
+    //   description: "A sports Bike",
+    //   releaseDate: "10-08-2010",
+    //   price: 200,
+    //   isActive: true,
+    //   imageUrl: "https://via.placeholder.com/150?text=Hornet",
+    // },
+    // {
+    //   productName: "Super splendor",
+    //   description: null,
+    //   releaseDate: "10-08-1980",
+    //   price: 75,
+    //   isActive: true,
 
-      imageUrl: "https://via.placeholder.com/150?text=Splendor",
-    },
-    {
-      productName: "Yamaha RX 100",
-      description: "Nostalgic !",
-      releaseDate: "10-08-1987",
-      price: 122,
-      isActive: false,
-      imageUrl: "https://via.placeholder.com/150?text=RX100",
-    },
-    {
-      productName: "Bajaj Pulsar",
-      description: "",
-      releaseDate: "10-08-1920",
-      price: 9,
-      isActive: false,
-      imageUrl: "https://via.placeholder.com/150?text=Pulsar",
-    },
+    //   imageUrl: "https://via.placeholder.com/150?text=Splendor",
+    // },
+    // {
+    //   productName: "Yamaha RX 100",
+    //   description: "Nostalgic !",
+    //   releaseDate: "10-08-1987",
+    //   price: 122,
+    //   isActive: false,
+    //   imageUrl: "https://via.placeholder.com/150?text=RX100",
+    // },
+    // {
+    //   productName: "Bajaj Pulsar",
+    //   description: "",
+    //   releaseDate: "10-08-1920",
+    //   price: 9,
+    //   isActive: false,
+    //   imageUrl: "https://via.placeholder.com/150?text=Pulsar",
+    // },
   ];
 
   actualBikes: IProduct[] = [...this.products];
@@ -89,39 +89,39 @@ export class ProductListComponent implements OnInit {
       );
     }
   }
-  getClasses(product: IProduct) {
-    // 'classname'
+  // getClasses(product: IProduct) {
+  //   // 'classname'
 
-    // 'class1 class2'
+  //   // 'class1 class2'
 
-    // ['class1', 'class2', 'class3']
+  //   // ['class1', 'class2', 'class3']
 
-    // {
-    //     'classname1':expression1 ,
-    //     'classname2':expression2 ,
-    // }
+  //   // {
+  //   //     'classname1':expression1 ,
+  //   //     'classname2':expression2 ,
+  //   // }
 
-    const is200 = product.price === 200;
-    if (is200) {
-      return {
-        "my-reen": is200,
-        bold: is200,
-      };
-    } else {
-      return {};
-    }
-  }
+  //   const is200 = product.price === 200;
+  //   if (is200) {
+  //     return {
+  //       "my-reen": is200,
+  //       bold: is200,
+  //     };
+  //   } else {
+  //     return {};
+  //   }
+  // }
 
-  getStyles(product: IProduct) {
-    if (product.price == 200) {
-      return {
-        color: "red",
-        "font-weight": "bold",
-      };
-    } else {
-      return {};
-    }
-  }
+  // getStyles(product: IProduct) {
+  //   if (product.price == 200) {
+  //     return {
+  //       color: "red",
+  //       "font-weight": "bold",
+  //     };
+  //   } else {
+  //     return {};
+  //   }
+  // }
 
   changeName() {
     this.products[0].productName = "Nexon";
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,9 +1,39 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from "@angular/core";
+import { IProduct } from "src/app/models/product.interface";
 
 @Component({
   selector: "app-product",
-  template: "<h2>Hello Inside Product Componnet</h2>",
+  templateUrl: "./product.component.html",
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, OnChanges {
   constructor() {}
+
+  @Input() productToBeRendered: IProduct;
+  @Input() canShowImage: boolean;
+
+  private counter: number = 0;
+
+  ngOnInit(): void {
+    console.log("Inside ProductComponent - ngOnInit()");
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("Inside ngOnChanges of Product Component", changes);
+  }
+
+  getStyles(product: IProduct) {
+    if (product.price == 200) {
+      return {
+        color: "red",
+        "font-weight": "bold",
+      };
+    } else {
+      return {};
+    }
+  }
 }
