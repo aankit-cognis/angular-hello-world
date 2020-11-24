b497d65fa2c2177d42298b4adc382c5a36e82556
SWAGAT KUMAR SWAIN <swagat@cognissolutions.com>
2020-11-24 19:59:27 +0530

    error handling in httpclient

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from "@angular/core";
 import { IProduct } from "../models/product.interface";
 import { Notyf } from "notyf";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { UtilityService } from "./utility.service";
+import { catchError } from "rxjs/operators";
 @Injectable({
   providedIn: "root",
 })
@@ -10,26 +12,42 @@ export class ProductService {
   lastDeletedProduct: string = "";
 
   private _baseUrl: string = "http://testapi.techriff.in/api";
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private utilityService: UtilityService
+  ) {}
 
   getProducts(): Observable<IProduct[]> {
     console.log("Inside Product Service ! Get Products Method()");
-    return this.http.get<IProduct[]>(`${this._baseUrl}/open/products`);
+    return this.http
+      .get<IProduct[]>(`${this._baseUrl}/open/products2`)
+      .pipe(catchError(this.handleError));
   }
   changeStatus(id: number, status: string): Observable<IProduct> {
     if (status == "reactivate") {
-      return this.http.post<IProduct>(
-        `${this._baseUrl}/open/products/${id}/reactivate`,
-        null
-      );
+      return this.http
+        .post<IProduct>(`${this._baseUrl}/open/products/${id}/reactivate`, null)
+        .pipe(catchError(this.handleError));
     } else {
-      return this.http.post<IProduct>(
-        `${this._baseUrl}/open/products/${id}/deactivate`,
-        null
-      );
+      return this.http
+        .post<IProduct>(`${this._baseUrl}/open/products/${id}/deactivate`, null)
+        .pipe(catchError(this.handleError));
     }
   }
   deleteProduct(id: number): Observable<IProduct> {
-    return this.http.delete<IProduct>(`${this._baseUrl}/open/products/${id}`);
+    return this.http
+      .delete<IProduct>(`${this._baseUrl}/open/products/${id}`)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorResponse: HttpErrorResponse) {
+    if (errorResponse.error instanceof ErrorEvent) {
+      console.error("Client Side Errors ", errorResponse.error);
+    } else {
+      console.error("Server Side Errors ", errorResponse.error);
+    }
+    return throwError(
+      "Sorry, We are having some issues now. Please get back later."
+    );
   }
 }
