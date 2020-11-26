bfc66ce13bad522ec80e89c689c1a4148ec59489
SWAGAT KUMAR SWAIN <swagat@cognissolutions.com>
2020-11-27 00:39:20 +0530

    form value

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: "app-register",
@@ -7,19 +8,19 @@ import { Component, OnInit } from "@angular/core";
 })
 export class RegisterComponent implements OnInit {
   originalData: any = {
-    fullName: "sasas",
-    email: "sas",
-    country: "india",
-    notes: "sasa",
-    isSubscribe: true,
-    gender: "male",
+    fullName: null,
+    email: null,
+    country: null,
+    notes: null,
+    isSubscribe: null,
+    gender: null,
   };
 
   data = { ...this.originalData };
 
   constructor() {}
   ngOnInit() {}
-  onSubmit(form) {
-    console.log("Inside Form", form.value);
+  onSubmit(form: NgForm) {
+    console.log("Inside Form", form.submitted);
   }
 }
