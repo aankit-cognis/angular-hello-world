30d378796a20f5dfd10704516eeaa2710f096aa0
SWAGAT KUMAR SWAIN <swagat@cognissolutions.com>
2020-11-28 11:08:59 +0530

    set and patch value using form group

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { FormControl, FormGroup, NgForm } from "@angular/forms";
 import { IRegister } from "src/app/models/register.interface";
 
 @Component({
@@ -8,24 +8,40 @@ import { IRegister } from "src/app/models/register.interface";
   styleUrls: ["./register.component.scss"],
 })
 export class RegisterComponent implements OnInit {
-  data: IRegister;
-  originalData: IRegister;
+  data: IRegister = {
+    fullName: "John Smith",
+    emailAddress: "",
+    country: "",
+    gender: "",
+    isSubscribe: false,
+    password: "",
+  };
 
+  registerForm: FormGroup;
   constructor() {}
 
   ngOnInit() {
-    //I am making an http call.
-    this.data = {
-      fullName: "",
-      emailAddress: "john@gmail.com",
-      country: "srilanka",
-      gender: "female",
-      isSubscribe: false,
-      password: "pssword@123",
-    };
-    this.originalData = { ...this.data };
+    this.registerForm = new FormGroup({
+      fullName: new FormControl(),
+      emailAddress: new FormControl(),
+      isSubscribe: new FormControl(false),
+    });
   }
-  submitForm(form: NgForm) {
-    console.log("Form SUbmitted ", form);
+  submitForm() {
+    console.log("Form SUbmitted ", this.registerForm);
+  }
+
+  fillWithDummyData() {
+    //API Call
+    let datafromservice = {
+      fullName: "Swagat",
+      emailAddress: "swagat@gmail.com",
+      is_Subscribed: true,
+    };
+    console.log("Fetching Data");
+    this.registerForm.patchValue({
+      fullName: datafromservice.fullName,
+      emailAddress: datafromservice.emailAddress,
+    });
   }
 }
