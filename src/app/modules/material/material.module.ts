f4be95f7f51a31437381dbe23228d0d7fcc9da20
SWAGAT KUMAR SWAIN <swagat@cognissolutions.com>
2020-12-03 18:51:32 +0530

    material theme

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MaterialComponent } from './material.component';
-
+import { NgModule } from "@angular/core";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatCardModule } from "@angular/material/card";
+import { MatGridListModule } from "@angular/material/grid-list";
 @NgModule({
-  imports: [
-    CommonModule
+  exports: [
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatGridListModule,
   ],
-  declarations: [MaterialComponent]
 })
-export class MaterialModule { }
+export class MaterialModule {}
