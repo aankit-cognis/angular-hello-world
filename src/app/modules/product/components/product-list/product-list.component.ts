c1eaf80105e7a729a5e354dec6cda25837d687f2
SWAGAT KUMAR SWAIN <swagat@cognissolutions.com>
2020-12-04 00:49:28 +0530

    table and paginator

--- src/app/modules/product/components/product-list/product-list.component.ts
+++ src/app/modules/product/components/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { LowerCasePipe, UpperCasePipe } from "@angular/common";
-import { Component, OnInit } from "@angular/core";
+import { AfterViewInit, Component, OnInit, ViewChild } from "@angular/core";
+import { MatPaginator } from "@angular/material/paginator";
 import { MatTableDataSource } from "@angular/material/table";
 import { IProduct } from "src/app/models/product.interface";
 import { IfNullOrEmpty } from "src/app/pipes/if-null-or-empty.pipe";
@@ -16,7 +17,7 @@ import { ProductService } from "src/app/services/product.service";
     `,
   ],
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, AfterViewInit {
   displayedColumns: string[] = [
     "productName",
     "description",
@@ -25,7 +26,7 @@ export class ProductListComponent implements OnInit {
     "price",
   ];
   dataSource: MatTableDataSource<IProduct>;
-
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   constructor(
     private upperCasePipe: UpperCasePipe,
     private lowerCasePipe: LowerCasePipe,
@@ -47,7 +48,7 @@ export class ProductListComponent implements OnInit {
   getTitle(): string {
     return "Hello from Method";
   }
-
+  ngAfterViewInit() {}
   // showImage(getImage('path', 'format), 'thumbnail');
 
   toggleImages(): void {
@@ -91,6 +92,7 @@ export class ProductListComponent implements OnInit {
         this.products = data;
         this.actualBikes = [...this.products];
         this.dataSource = new MatTableDataSource<IProduct>(data);
+        this.dataSource.paginator = this.paginator;
       },
       (error: any) => {
         console.error("An error occured", error);
